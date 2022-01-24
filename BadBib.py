a55dde92bf70cacb9d473d1501b8b42cabc1fbba
Bitleble <91606576+Bitleble@users.noreply.github.com>
2022-01-25 02:22:48 +0300

    Req

--- BadBib.py
+++ BadBib.py
@@ -8,19 +8,33 @@ f = open('Hihish.txt', 'r')
 jokes = f.read().split('___')
 f.close()
 
+h = []
+h1 = []
+
 bot = telebot.TeleBot('5205867083:AAGG27PmKJJ2-uxNwh7QcsADzkR56ldckRg')
+
+
 @bot.message_handler(commands=["start"])
-def start(m, res=False):
+def start(m):
     bot.send_message(m.chat.id, '/help для большей информации')
+
+
 @bot.message_handler(commands=["help"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Доступные команды:'+'\n'+ '/hohma - авторские анекдоты '+'\n'+'/hohma1 - анекдоты с анекдоты.ру(официально самый артхаусный артхаус)'
-                                +'\n'+'/recipe: Поиск рецептов по написанным ингридиентам')
+def start(m):
+    bot.send_message(m.chat.id, '(HELP....HEГР)' + '\n'
+                     + 'Доступные команды:' +
+                     '\n' + '/hohma - авторские анекдоты ' +
+                     '\n' + '/hohma1 - анекдоты с анекдоты.ру(официально самый артхаусный артхаус)' +
+                     '\n' + '/recipe: Поиск рецептов по написанным ингридиентам')
+
+
 @bot.message_handler(commands=['hohma'])
 def hihi(message):
-    i = randint(1,7)
-    bot.send_message(message.chat.id, 'Партия держать меня в подвале, я работать за миска рис, посмейтесь, и я буду работать за два миска рис. ')
+    i = randint(1, 7)
+    bot.send_message(message.chat.id, 'Партия держать меня в подвале, я работать за миска рис, посмейтесь'
+                                      ', и я буду работать за два миска рис. ')
     bot.send_message(message.chat.id, jokes[i])
+
 
 @bot.message_handler(commands=['hohma1'])
 def haha(message):
@@ -31,6 +45,7 @@ def haha(message):
     soup = BeautifulSoup(inin.text, 'lxml')
     s = soup.find_all('p')
     bot.send_message(message.chat.id, s[i].text)
+
 
 @bot.message_handler(commands=["recipe"])
 def handle_text(message):
@@ -43,17 +58,32 @@ def recipe(message):
     s = requests.Session()
     inin = s.get('https://povar.ru/xmlsearch?query={}'.format(message.text))
     soup = BeautifulSoup(inin.text, 'lxml')
-    q = soup.find_all('a',class_='listRecipieTitle')
-    i = 0
+    q = soup.find_all('a', class_='listRecipieTitle')
     i1 = 0
-    for word in q:
-        i += 1
-        bot.send_message(message.chat.id, str(i) + ' ' + word.text)
-    for link in soup.find_all('a',class_='listRecipieTitle'):
-        if link.get('href') != None:
+    for link in soup.find_all('a', class_='listRecipieTitle'):
+        if link.get('href') is not None:
             i1 += 1
-            bot.send_message(message.chat.id, str(i1)+' '+' https://povar.ru'+str(link.get('href')))
+            if len(h1) != 0:
+                    if ('https://povar.ru' + str(link.get('href'))) in h1:
+                        i1 -= 1
+                        continue
+                    else:
+                        bot.send_message(message.chat.id, str(i1) + ' ' + ' https://povar.ru' + str(link.get('href')))
+                        h.append('https://povar.ru' + str(link.get('href')))
+            else:
+                bot.send_message(message.chat.id, str(i1) + ' ' + ' https://povar.ru' + str(link.get('href')))
+                h.append('https://povar.ru' + str(link.get('href')))
+    send = bot.send_message(message.chat.id, 'Введите номер просмотренного рецепта, для его исключения в дальнейшем поиске')
+    bot.register_next_step_handler(send, delmass)
 
+def delmass(message):
+    delindex = int(message.text) - 1
+    h1.append(h[delindex])
+    h.clear()
 
+@bot.message_handler(commands=["Povtor"])
+def povtor(m):
+    for i in range(len(h1)):
+        bot.send_message(m.chat.id,h1[i])
 
 bot.polling(none_stop=True, interval=0)
